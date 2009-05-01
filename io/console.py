1bad1a9ba0778102819cee7a30f8be64a97eeb81
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-05-01 17:51:40 +0800

    console input

---
+++ io/console.py
@@ -0,0 +1,6 @@
+# http://www.penzilla.net/tutorials/python/consoleio/
+
+print "Hello User, you're lookin' fine!"
+print "Can I get your phone number?"
+phonenumber = raw_input("> ")
+print phonenumber
