2d7d8ad70a2526a3e7ad340f0cecbfd769edf60e
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2011-12-09 14:55:12 +0800

    word_tokenize raw text

--- nltk/word_tokenize.py
+++ nltk/word_tokenize.py
@@ -5,3 +5,8 @@ para = "Hello, world! It's good to see you. Thanks for buying this book."
 sents = sent_tokenize(para)
 sents = [word_tokenize(sentence) for sentence in sents]
 print sents
+
+print "****"
+
+words = word_tokenize(para)
+print words
