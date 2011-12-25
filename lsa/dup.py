80905a095a60132bce4faa17fd4ac960f4b27852
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2011-12-25 15:04:23 +0800

    manually compute cosine distance

--- lsa/dup.py
+++ lsa/dup.py
@@ -5,8 +5,9 @@
 
 from numpy import *
 from nltk.cluster.util import *
-from numpy.linalg import svd
+from numpy.linalg import svd, norm
 import matplotlib.pyplot as plt
+from math import acos
 
 A = array([
   [1,1],
@@ -24,6 +25,16 @@ S2 = S[0:2]
 V2 = V[0:2,:]
 
 A2 = dot(dot(U2,diag(S2)), V2)
-print cosine_distance(A2[:,0], A2[:,1]) #0.0
+print A2[:,0]
+print A2[:,1]
+
+def cosine_similarity(a, b):
+  return dot(a, b) / (norm(a) * norm(b))
+  
+print cosine_similarity(A2[:,0], A2[:,1])
+
+#print dot(A2[:,0], A2[:,0]) / norm(A2[:,0]) * norm(A2[:,1])
+#print cosine_distance([1,2],[1,2])
+#print dot(A2[:,0], A2[:,1])
 
 #print A2
